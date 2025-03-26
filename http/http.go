84ff0d16a9cbca6464b2de1d5a51851b6e2c2c26
Jeremías <jrmsdev@gmail.com>
2025-03-26 20:04:17 -0300

    http: listen and serve

--- http/http.go
+++ http/http.go
@@ -4,9 +4,18 @@
 package http
 
 import (
+	"log"
+	"net/http"
+
 	_ "chesslovaquia.github.io/go/clvq/cfg"
 )
 
 func Main(port string) error {
+	log.Printf("starting http server on port: %s", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		return err
+	}
+
 	return nil
 }
