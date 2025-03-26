dc811d85c25a99a2d3791d96865a24d761b60727
Jeremías <jrmsdev@gmail.com>
2025-03-26 20:15:21 -0300

    http: main handler

--- http/http.go
+++ http/http.go
@@ -6,12 +6,28 @@ package http
 import (
 	"log"
 	"net/http"
+	"path"
+	"strings"
 
 	_ "chesslovaquia.github.io/go/clvq/cfg"
 )
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	reqPath := path.Clean(r.URL.Path)
+	if strings.HasSuffix(reqPath, "/") {
+		reqPath = path.Join(reqPath, "index.html")
+	}
+	ext := path.Ext(reqPath)
+	if ext == "" {
+		reqPath = path.Join(reqPath, "index.html")
+		ext = path.Ext(reqPath)
+	}
+}
+
 func Main(port string) error {
 	log.Printf("starting http server on port: %s", port)
+
+	http.HandleFunc("/", Handler)
 
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		return err
