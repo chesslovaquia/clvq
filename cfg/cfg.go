e45e9580e8257c0ff2edeb2796ba92a160c959df
Jeremías <jrmsdev@gmail.com>
2025-03-27 12:18:35 -0300

    cfg: Load and Save

--- cfg/cfg.go
+++ cfg/cfg.go
@@ -3,6 +3,11 @@
 
 package cfg
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	path string
 }
@@ -17,7 +22,43 @@ func newConfig() *Config {
 	return &Config{}
 }
 
+func Save(path string) error {
+	fh, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+	encoder := json.NewEncoder(fh)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(cfg); err != nil {
+		return err
+	}
+	return nil
+}
+
+var autoSave bool = true
+
 func Load(path string) error {
+	if blob, err := os.ReadFile(path); err != nil {
+		if os.IsNotExist(err) {
+			if autoSave {
+				autoSave = false
+				if err := Save(path); err != nil {
+					return err
+				} else {
+					return Load(path)
+				}
+			} else {
+				return err
+			}
+		} else {
+			return err
+		}
+	} else {
+		if err := json.Unmarshal(blob, cfg); err != nil {
+			return err
+		}
+	}
 	cfg.path = path
 	return nil
 }
